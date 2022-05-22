e559ce3a4727fc238a7e15e007bac8126ee33df5
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-05-22 16:44:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from pyrogram import Client
 
+from pytgcalls import PyTgCalls
+from pytgcalls import StreamType
+from pytgcalls import idle
+from pytgcalls.media_devices import MediaDevices
+from pytgcalls.types import CaptureVideoDesktop
+
+app = Client(
+    'py-tgcalls',
+    api_id=123456789,
+    api_hash='abcdef12345',
+)
+
+call_py = PyTgCalls(app)
+call_py.start()
+
+print("VCMANAGER STARTED!! ") 
+
+idle()
